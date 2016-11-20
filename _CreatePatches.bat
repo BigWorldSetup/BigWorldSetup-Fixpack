cc2b8f6c5df6d99555f15b7a9d2073ec927aaef3
agb1 <agb1@shsforums.net>
2016-11-20 15:39:39 -0500

    Renamed BiG_World_Fixpack.tp2 to _ApplyPatches.tp2, fixed a typo at line 185 that prevented _depends/_depends_not subfolders from being processed, and switched to unique array variables to avoid processing elements left over from previous arrays Renamed CreatePatches.tp2 to _CreatePatches.tp2, renamed marker file (added _ prefix), corrected _diffgawk path, and revised usage instructions Added _ApplyPatches.bat and _CreatePatches.bat convenience scripts, using --nogame to allow these scripts to run outside a game folder

---
+++ _CreatePatches.bat
@@ -0,0 +1,3 @@
+:: Run this in the same folder as _CreatePatches.tp2 and WeiDU.exe (not included) - usually your game folder, but could be elsewhere
+
+weidu --nogame --log _CreatePatches.debug _CreatePatches.tp2
