a78fd7abc2fa9ead50adfb6283ac349452cb4e0c
agb1 <agb1@shsforums.net>
2016-11-29 03:03:07 -0500

    More robust patching script

--- _ApplyPatches.bat
+++ _ApplyPatches.bat
@@ -1,3 +1,3 @@
 :: Run this in the same folder as _ApplyPatches.tp2 and WeiDU.exe (not included) - usually your game folder, but could be elsewhere
 
-weidu --nogame --log _ApplyPatches.debug _ApplyPatches.tp2
+weidu --nogame --no-pause-at-exit --logapp _ApplyPatches.debug _ApplyPatches.tp2
