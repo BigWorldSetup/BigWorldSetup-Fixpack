eeced40b9bbc4386dd1a3e1fe97cf35e219f32d8
agb1 <agb1@shsforums.net>
2016-11-29 23:01:37 -0500

    re-enable patching script - oops

--- _ApplyPatches.bat
+++ _ApplyPatches.bat
@@ -1,3 +1,3 @@
 :: Run this in the same folder as _ApplyPatches.tp2 and WeiDU.exe (not included) - usually your game folder, but could be elsewhere
 
-weidu --nogame --no-pause-at-exit --logapp _ApplyPatches.debug _ApplyPatches.tp2
+weidu --nogame --no-exit-pause --logapp --log _ApplyPatches.debug _ApplyPatches.tp2
