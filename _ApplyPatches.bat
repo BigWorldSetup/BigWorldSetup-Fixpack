16d87141df743d255a30a63609af6b45c7f3702e
ALIENQuake <alienquake@hotmail.com>
2016-12-09 11:43:39 +0100

    fixed execution directory

--- _ApplyPatches.bat
+++ _ApplyPatches.bat
@@ -1,3 +1,4 @@
 :: Run this in the same folder as _ApplyPatches.tp2 and WeiDU.exe (not included) - usually your game folder, but could be elsewhere
-
+SETLOCAL
+cd /d %~dp0
 weidu --nogame --no-exit-pause --logapp --log _ApplyPatches.debug _ApplyPatches.tp2
--- _CreatePatches.bat
+++ _CreatePatches.bat
@@ -1,3 +1,4 @@
 :: Run this in the same folder as _CreatePatches.tp2 and WeiDU.exe (not included) - usually your game folder, but could be elsewhere
-
+SETLOCAL
+cd /d %~dp0
 weidu --nogame --log _CreatePatches.debug _CreatePatches.tp2
