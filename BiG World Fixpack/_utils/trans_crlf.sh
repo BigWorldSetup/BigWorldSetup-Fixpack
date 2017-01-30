77ea454dbc8ab07ac1b8bf81d08a2550c7d1d806
agb1 <agb1@shsforums.net>
2017-01-30 09:56:14 -0500

    Contribution from AstroBryGuy:  Added CRLF->LF translation script for OSX and Linux.  The patch utility on Linux/OSX expects lines to end only with line feed (LF). Windows uses carriage-return line-feed (CRLF). patch tries to be smart and automatically converts the .patch files to LF, stripping out the CR. However, a mod's files will still use CRLF, so patch will fail. This adds a call to a shell script that pre-processes the file to be patched, changing CRLF to LF.

---
+++ BiG World Fixpack/_utils/trans_crlf.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+patchfile=$1
+
+files=($(awk '/^--- / {print $2}' $patchfile))
+for filename in ${files[*]};
+do
+	if [[ $filename = *[^[:space:]]* && -f "$filename" ]]; then
+		tr -d '\r' < $filename > "$filename".lf
+    	echo "Translating CRLF->LF for $filename"
+    	echo "Translating CRLF->LF for $filename" >> _ApplyPatches_verbose.log
+		if [ "$?" -ne 0 ]; then
+    		echo "CRLF->LF translation failed for $filename"
+    		echo "CRLF->LF translation failed for $filename" >> _ApplyPatches_verbose.log
+		else
+    		echo "CRLF->LF translation succeeded for $filename"
+    		echo "CRLF->LF translation succeeded for $filename" >> _ApplyPatches_verbose.log
+			/bin/mv -f "$filename".lf $filename
+		fi 
+	else
+		echo "Invalid filename: $filename"
+		echo "Invalid filename: $filename" >> _ApplyPatches_verbose.log
+	fi
+done
